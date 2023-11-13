c8979888be40914dce6d5b4c344f582c942cd4a9
LAPTOP-5Q44CSU7\reika <reikal951@gmail.com>
2023-11-13 19:49:38 +0200

    Created schemas for User & Movie

---
+++ db/schemas/Movie.ts
@@ -0,0 +1,26 @@
+import { Schema } from "mongoose";
+
+const movieSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+        minLength: 1,
+        maxLength: 100,
+    },
+    imageURL: {
+        type: String,
+        required: true,
+    },
+    imbURL: {
+        type: String,
+
+    },
+    description: {
+        type: String,
+        min: 5,
+        max: 200,
+    }
+    
+})
+
+export default movieSchema
---
+++ db/schemas/User.ts
@@ -0,0 +1,25 @@
+import { Schema } from "mongoose";
+require("./Movie")
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        minLength: 4,
+        maxLength: 25,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6,
+        maxLength: 100,
+    },
+    favMovies: [{
+        type: Schema.Types.ObjectId,
+        ref: "Movie",
+        default: [],
+    }]
+})
+
+export default userSchema
